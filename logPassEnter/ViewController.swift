1b3ea682501105b3ac8fdb52b03de6bad399a9bf
Ilya <leynsboro@gmail.com>
2022-04-13 16:24:33 +0300

    Initial Commit

---
+++ logPassEnter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  logPassEnter
+//
+//  Created by Илья Гусаров on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
